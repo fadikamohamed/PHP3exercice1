430899dfb709026dc18b4a097a0875e652f19ede
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:50:42 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$incre = 0;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 1</title>
+    </head>
+    <body>
+        <?php
+        while ($incrementation <= 10) {
+            echo $incrementation;
+            $incrementation++;
+        }
+        ?>
+    </body>
+</html>
